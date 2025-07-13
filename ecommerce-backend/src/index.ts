1f07707177b072ed7fbf04ac1cc01d779f7bf77f
CAPTAIN-BLACTRACT <iiser.ayush@gmail.com>
2025-07-13 07:06:58 +0000

    completed chunk 3.1 that is implemented find products from mongo db using mongoose

--- ecommerce-backend/src/index.ts
+++ ecommerce-backend/src/index.ts
@@ -1,7 +1,9 @@
-import express from "express";
+import express, {Request,Response} from "express";
 import cors from "cors";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
+import {Product} from "./models/Product";
+
 
 dotenv.config();
 connectDB();
@@ -10,6 +12,18 @@ const port = process.env.PORT || 3000;
 
 app.use(express.json());
 app.use(cors());
+
+app.get('/api/products', async(req,res)=>{
+  try { 
+    const products = await Product.find({});
+    res.status(200).json(products);
+    }
+    catch(error:any){
+        console.error('error fetching products from database',error);
+        res.status(500).json({message:'error fetching products from database',error: error.message});
+    }
+    
+});
 
 app.get('/',(req,res) =>{
 
---
+++ ecommerce-backend/src/models/Product.ts
@@ -0,0 +1,19 @@
+import mongoose, {Document,Schema, Model} from "mongoose";
+
+export interface IProduct extends Document{
+    name:string;
+    description:string;
+    price:number;
+    imageUrl:string;
+}
+
+const ProductSchema: Schema = new Schema({
+    name: {type: String, required: true, trim: true },
+    description: {type:  String , required: true},
+    price: {type: Number , required: true, min:0},
+    imageUrl: {type: String, required: true},
+},{timestamps:true
+});
+
+const Product : Model<IProduct> = mongoose.model<IProduct>('Product',ProductSchema);
+export {Product};
