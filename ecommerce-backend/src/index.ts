2ea00bbd7e1bd6cc4a0e6552f1e28796327e120b
CAPTAIN-BLACTRACT <iiser.ayush@gmail.com>
2025-07-14 07:13:09 +0000

    completed chunk 3.2 of adding an api/product/:id route which will help us find the product with id

--- ecommerce-backend/src/index.ts
+++ ecommerce-backend/src/index.ts
@@ -3,6 +3,7 @@ import cors from "cors";
 import connectDB from "./config/db";
 import dotenv from "dotenv";
 import {Product} from "./models/Product";
+import mongoose from "mongoose";
 
 
 dotenv.config();
@@ -23,6 +24,30 @@ app.get('/api/products', async(req,res)=>{
         res.status(500).json({message:'error fetching products from database',error: error.message});
     }
     
+});
+
+
+app.get('/api/products/:id', async(req,res )=>{
+
+    try{
+        const productId =  req.params.id;
+
+        const product =  await Product.findById(productId);
+
+        if(!product)
+        {
+            res.status(404).json({message:'Product not found'});
+        }
+        res.status(200).json(product);
+    }
+    catch(error){
+        console.error(`Error fetching product by id : ${req.params.id}`, error);
+
+        if(error instanceof mongoose.Error.CastError){
+            return res.status(400).json({message:'invalid id error'});
+        }
+        res.status(500).json({message:'internal server error while finding product by id'});
+    }
 });
 
 app.get('/',(req,res) =>{
