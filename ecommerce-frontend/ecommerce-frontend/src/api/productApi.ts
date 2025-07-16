8c3f2fc45644891e1b9dd348ee1ffd09c4cc96e3
CAPTAIN-BLACTRACT <iiser.ayush@gmail.com>
2025-07-16 02:59:12 +0000

    initiated frontend task -> create folder frontend -> installed create-react-app and axios -> the project is using typescript

---
+++ ecommerce-frontend/ecommerce-frontend/src/api/productApi.ts
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import { Product } from '../types/Product';
+
+const API = axios.create({
+    baseURL : 'http:/localhost:3000/api',
+});
+
+export const getAllProducts = async() : Promise<Product[]> => {
+    const response = await API.get<Product[]>('/products');
+    return response.data;
+}
+
+export const getProductById = async(id:string) : Promise<Product> => {
+    const response = await API.get<Product>(`/products/${id}`);
+    return response.data;
+}
---
+++ ecommerce-frontend/ecommerce-frontend/src/types/Product.ts
@@ -0,0 +1,9 @@
+export interface Product{
+    _id : string;
+    name: string;
+    description : string;
+    price : number;
+    imageUrl: string;
+    createdAt : string;
+    updatedAt : string;
+}
