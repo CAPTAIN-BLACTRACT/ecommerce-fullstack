8871988787ba6e6a1740c337db5190143f8bceb4
CAPTAIN-BLACTRACT <iiser.ayush@gmail.com>
2025-07-20 18:18:09 +0000

    intalled router-dom package and created handleAddToCart function while also removing other errors of app.tsx

--- ecommerce-frontend/ecommerce-frontend/src/App.tsx
+++ ecommerce-frontend/ecommerce-frontend/src/App.tsx
@@ -1,12 +1,46 @@
 import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import ProductList from "./components/ProductList";
+
 import "./App.css";
+import { Product } from "./types/Product";
+
+interface CartItem{
+
+    _id: string;
+    name: string;
+    price: number;
+    quantity:number;
+
+}
 
 function App(){
-  const {cartItems,setCartItems}= useState<any[]>([]);
+  const [cartItems , setCartItems] = useState<CartItem[]>([]);
   const totalCartItems = cartItems.reduce((acc,item)=>acc+item.quantity,0);
 
+  const handleAddToCart =  (productToAdd : Product)=>{
+    setCartItems((prevItems)=>{
+      const existingItem = prevItems.find(item=>item._id===productToAdd._id);
+
+      if(existingItem)
+      {
+        return prevItems.map(item=>
+          item._id===productToAdd._id ? {...item, quantity:item.quantity+1} : item
+        );
+      } 
+      else{
+        return [...prevItems,{
+          _id: productToAdd._id,
+          name: productToAdd.name,
+          price: productToAdd.price,
+          quantity: 1
+        }];
+      }
+    });
+  };
   return(
+    <Router>{}
     <div className="App">
       <Header cartItemCount={totalCartItems} />
       <main className="app-content" >
@@ -14,6 +48,7 @@ function App(){
         <p>Content goes here....</p>
       </main>
     </div>
+    </Router>
   );
 }
 
