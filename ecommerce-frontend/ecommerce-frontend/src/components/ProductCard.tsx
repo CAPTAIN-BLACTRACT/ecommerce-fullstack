6fbecbd9414356a29e6c72b7d18c75ff1e1a2c3f
CAPTAIN-BLACTRACT <iiser.ayush@gmail.com>
2025-07-27 05:55:28 +0000

    added the rest of the code so that it can work

--- ecommerce-frontend/ecommerce-frontend/src/components/ProductCard.tsx
+++ ecommerce-frontend/ecommerce-frontend/src/components/ProductCard.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { Product } from "../types/Product";
 import './ProductCard.css';
+import { Link } from "react-router-dom";
+import ProductDetail from "./ProductDetail";
+import { classicNameResolver } from "typescript";
 
 interface ProductCardProps{
     product : Product;
 }
+
+
+
 
 const ProductCard : React.FC<ProductCardProps> = ({product}) => {
 
@@ -15,7 +21,9 @@ const ProductCard : React.FC<ProductCardProps> = ({product}) => {
                 <h3 className="product-card-name">{product.name}</h3>
                 <p className="product-card-price" >${product.price.toFixed(2)}</p>
                 {}
+                <Link to={`/product/${product._id}`} className = "product-card-link">
                 <button>View Detail</button>
+                </Link>
             </div>
         </div>
     );
